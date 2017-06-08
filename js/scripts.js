1ed9d2406984fe57c6f13d7dc74d9f8b7c8c76d6
Banjamin Hampikian and Larry Taylor <banjamin@>
2017-06-08 10:52:51 -0700

    show NaN for input numerals greater than 3999

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
+  $(".converter-form").submit(function(event){
+    event.preventDefault();
 
+    function convertToRN(number) {
+      if (number >= 4000) {
+        return NaN;
+      } else {
+        return number;
+      }
+    }
+
+
+
+
+
+
+
+
+
+    var input = $("input#number").val();
+    console.log(input);
+    var result = convertToRN(input);
+    $("#result").text(result);
+
+  });
 });
